6d6ffc032185f1868ce8519a472258f4acc9ddac
Litha2004 <lmkunqwa@montgomerycollege.edu>
2025-02-26 10:06:30 -0500

    Update Patient.java

--- Patient.java
+++ Patient.java
@@ -2,7 +2,7 @@
  * Class: CMSC203 
  * Instructor:Khandan Monshi
  * Description: (Give a brief description for each Class)
- * Due: 10/08/2024
+ * Due: 02/26/2025
  * Platform/compiler:
  * I pledge that I have completed the programming 
  * assignment independently. I have not copied the code 
@@ -10,25 +10,38 @@
  * to any student.
    Print your Name here: Lithalethu Mkunqwana__________
 */
-
-public class Patient {
+public class patient {
+    // Fields
     private String firstName;
     private String middleName;
     private String lastName;
-    private String address;
+    private String streetAddress;
     private String city;
     private String state;
     private String zipCode;
     private String phoneNumber;
     private String emergencyContactName;
     private String emergencyContactPhone;
-
-    // Full constructor
-    public Patient(String firstName, String middleName, String lastName, String address, String city, String state, String zipCode, String phoneNumber, String emergencyContactName, String emergencyContactPhone) {
+    
+    // No-arg constructor
+    public patient() {
+    }
+    
+    // Parametrized constructor for name only
+    public patient(String firstName, String middleName, String lastName) {
         this.firstName = firstName;
         this.middleName = middleName;
         this.lastName = lastName;
-        this.address = address;
+    }
+    
+    // Parametrized constructor for all attributes
+    public patient(String firstName, String middleName, String lastName, String streetAddress, String city, 
+                   String state, String zipCode, String phoneNumber, 
+                   String emergencyContactName, String emergencyContactPhone) {
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.lastName = lastName;
+        this.streetAddress = streetAddress;
         this.city = city;
         this.state = state;
         this.zipCode = zipCode;
@@ -36,12 +49,110 @@ public class Patient {
         this.emergencyContactName = emergencyContactName;
         this.emergencyContactPhone = emergencyContactPhone;
     }
+    
+    // Accessors (getters)
+    public String getFirstName() {
+        return firstName;
+    }
 
-    // Other methods like accessors and toString method
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getStreetAddress() {
+        return streetAddress;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getEmergencyContactName() {
+        return emergencyContactName;
+    }
+
+    public String getEmergencyContactPhone() {
+        return emergencyContactPhone;
+    }
+    
+    // Mutators (setters)
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setStreetAddress(String streetAddress) {
+        this.streetAddress = streetAddress;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setEmergencyContactName(String emergencyContactName) {
+        this.emergencyContactName = emergencyContactName;
+    }
+
+    public void setEmergencyContactPhone(String emergencyContactPhone) {
+        this.emergencyContactPhone = emergencyContactPhone;
+    }
+    
+    // Method to build full name
+    public String buildFullName() {
+        return firstName + " " + middleName + " " + lastName;
+    }
+    
+    // Method to build full address
+    public String buildAddress() {
+        return streetAddress + " " + city + " " + state + " " + zipCode;
+    }
+    
+    // Method to build emergency contact
+    public String buildEmergencyContact() {
+        return emergencyContactName + " " + emergencyContactPhone;
+    }
+    
+    // toString method
+    @Override
     public String toString() {
-        return "Name: " + firstName + " " + middleName + " " + lastName + "\n" +
-               "Address: " + address + ", " + city + ", " + state + " " + zipCode + "\n" +
-               "Phone: " + phoneNumber + "\n" +
-               "Emergency Contact: " + emergencyContactName + " (" + emergencyContactPhone + ")";
+        return "Full Name: " + buildFullName() + "\n" +
+               "Address: " + buildAddress() + "\n" +
+               "Phone Number: " + phoneNumber + "\n" +
+               "Emergency Contact: " + buildEmergencyContact();
     }
 }
